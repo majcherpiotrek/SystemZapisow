7bd0d56c7a57eba3a580acafe0fa6ea509e6096f
vins790 <insene790@gmail.com>
2016-12-06 11:49:07 +0100

    Dodałem println( "bla" );

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -107,6 +107,7 @@ public class Main extends Application {
 
         while(input.hasNextLine()) {
             line = input.nextLine();
+            System.out.println("bla");
             if(line.equals(loginLine))
                 return ValidationOutput.admin;
         }
